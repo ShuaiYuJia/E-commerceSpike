03072f6906452bd60ecfdc964cad1242457d702d
ShuaiShuai <1223086236@qq.com>
2021-10-30 14:06:47 +0800

    添加陕西

--- src/hello.java
+++ src/hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("china");
+        System.out.println("shanxi");
     }
 }
